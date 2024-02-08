d774f99f1801fd76a3c364498418ac1a726b8e3a
Natalia Ahmad <na906@nova.edu>
2024-02-07 21:59:32 -0500

    Fixed bugs in JS code

--- functions-problems/Problem 1-Longest Word/script.js
+++ functions-problems/Problem 1-Longest Word/script.js
@@ -1,31 +1,31 @@
 function testString(sampleString) {
   
-    if(sampleString && sampleString.length > 0 && typeof sampleString === 'string'){
-      let longestWord = 0;
-      let finalWord;
-      let splitString = sampleString.split(" ");
-      
-      for(i = 0; i < splitString.length; i++) {
-        finalWord = splitString[i].replace(/[^a-zA-Z]/g, "");
-        if(finalWord.length > longestWord) {
-          longestWord = finalWord.length;
-        }
+  if(sampleString && sampleString.length > 0 && typeof sampleString === 'string'){
+    let longestWord = 0;
+    let finalWord;
+    let splitString = sampleString.split(" ");
+    
+    for(i = 0; i < splitString.length; i++) {
+      finalWord = splitString[i].replace(/[^a-zA-Z]/g, "");
+      if(finalWord.length > longestWord) {
+        longestWord = finalWord.length;
       }
-      if(finalWord.length > 0) {
-        console.log('The longest word is of length ' + longestWord);
-      }
-      else {
-        console.log("Input either empty, not the right type or not a word");
-      }
-      return;    
     }
-  else {
-    console.log("Input either empty, not the right type or not a word");
+    if(finalWord.length > 0) {
+      return 'The longest word is of length ' + longestWord;
+    }
+    else {
+      return "Input either empty, not the right type or not a word";
+    }   
   }
+else {
+  return "Input either empty, not the right type or not a word";
+}
 }
 
-testString(2445); 
-testString("");
-testString("2445"); 
-testString("H111oooooow are you doinggg!!"); 
-testString("How old 34areeee yo#u");
+console.log(testString());
+console.log(testString(2445)); 
+console.log(testString(""));
+console.log(testString("2445")); 
+console.log(testString("H111oooooow are you doinggg!!")); 
+console.log(testString("How old 34areeee yo#u"));
