3b05fbde4df9cac944cbf1bf1d0cf07525dcd8b0
Natalia Ahmad <na906@nova.edu>
2024-02-19 22:21:39 -0500

    Refined/reduced JS code

--- functions-problems/Problem 2-Number of Vowels/script.js
+++ functions-problems/Problem 2-Number of Vowels/script.js
@@ -1,35 +1,33 @@
-function howManyVowels(sampleString) {                                       // function with string as a parameter
-    let singleLetters = [];                                                  // initializing an empty array that'll hold each letter in the sample string
-    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];       // an array of vowels that we need to compare our single letters array with
-    let vowelCounter = 0;                                                    // counter to count how many vowels in the sample string
-    let trimmedString;                                                       // variable to hold sampleString without special characters, spaces, and digits
+function howManyVowels(sampleString) {                                    // function with string as a parameter        
+  let vowelCounter = 0;                                                   // counter to count how many vowels in the sample string
+  let trimmedString;                                                      // variable to hold sampleString without special characters, spaces, and digits
   
-    if(!sampleString || typeof sampleString !== 'string') {                  // condition to check if an argument is not given or teh argument is not of the string datatype 
-      return "Input either empty or not a string";                           // if condition is true, we give this output
-    }
+  if(!sampleString || typeof sampleString !== 'string') {                 // condition to check if an argument is not given or teh argument is not of the string datatype
+    return "Input either empty or not a string";                          // if condition is true, we give this output 
+  }
+
+  let lowerCaseString = sampleString.toLowerCase();                       // converting sample string to lower case letters for vowel comparison
+  trimmedString = lowerCaseString.replace(/[^a-zA-Z]/g, "");              // now, I trim the argument string so it doesn't contain special characters, spaces, and digits   
   
-    trimmedString = sampleString.replace(/[^a-zA-Z]/g, "");                  // now, I trim the argument string so it doesn't contain special characters, spaces, and digits
-    
-    if(trimmedString.length === 0) {                                         // if condition to check if the length of the sample string after trimming is 0 
-        return "Input either empty or not a string";                         // if the length is 0, we return this statement
-    }
-  
-    if(trimmedString.length > 0) {                                           // checking if the length of the trimmed string is greater than 0
-      console.log('Sample string: ' + sampleString);                         // then, we print the sample string first           
-  
-      for(i = 0; i < trimmedString.length; i++) {                            // Next, we loop through the trimmed string and push each letter to an array
-        singleLetters.push(trimmedString[i]);                                // the array of these letters is called singleLetters
+  if(trimmedString.length === 0) {                                        // if condition to check if the length of the sample string after trimming is 0  
+      return "Input either empty or not a string";                        // if the length is 0, we return this statement
+  }
+
+  else {                                                                  // if the length of sample string after trimming it is > 0, we run the statements below
+    console.log('Sample string: ' + sampleString);                        // print the sample string first            
+
+    for(i = 0; i < trimmedString.length; i++) {                           // for loop to go over each letter in trimmed sample string
+      
+      if(trimmedString[i] === 'a' || trimmedString[i] === 'e' ||          // checking if the letter at index iis equal to any of the vowels
+         trimmedString[i] === 'i' || trimmedString[i] === 'o' || 
+         trimmedString[i] === 'u') {
+        
+          vowelCounter++;                                                 // if there is a vowel in the sample string, I add 1 to the vowel counter 
       }
-  
-      for(j = 0; j < singleLetters.length; j++) {                            // Now, I go through each single letter in the singleLetters array and compare it with each letter in the vowels array            
-        for(k = 0; k < vowels.length; k++) {
-          if(singleLetters[j] === vowels[k]) {                               // if a letter in the singleLetters array is stritcly same as a letter in vowels array
-            vowelCounter++;                                                  // then we add 1 to the vowelCounter
-          }
-         }
-      }
-      return "Number of vowels in string: " + vowelCounter;                  // And, return the number in vowelCounter variable
     }
+
+    return "Number of vowels in string: " + vowelCounter;                 // returning the number in vowelCounter variable
+  }
 
 }
 
@@ -38,23 +36,23 @@ console.log(howManyVowels(123));
 console.log(howManyVowels('2223 !!!!!'));
 console.log(howManyVowels('     What a lovely day!'));
 console.log(howManyVowels(''));
-console.log(howManyVowels('2223Wolly sang a song!'));
+console.log(howManyVowels('2223Wolly sang a sOng EEUa!'));
     
 
 /* OUTPUT 
 
-"Input either empty or not a string"            // 1st function call
+"Input either empty or not a string"               // 1st function call
 
-"Input either empty or not a string"            // 2nd function call
+"Input either empty or not a string"               // 2nd function call
 
-"Input either empty or not a string"            // 3rd function call
+"Input either empty or not a string"               // 3rd function call
 
-"Sample string:      What a lovely day!"        // 4th function call
+"Sample string:      What a lovely day!"           // 4th function call
 "Number of vowels in string: 5"
 
-"Input either empty or not a string"            // 5th function call
+"Input either empty or not a string"               // 5th function call
 
-"Sample string: 2223Wolly sang a song!"         // 6th function call
-"Number of vowels in string: 4"
+"Sample string: 2223Wolly sang a sOng EEUa!"       // 6th function call
+"Number of vowels in string: 8"
 
 */
