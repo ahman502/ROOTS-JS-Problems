524b858e3041cf1c23adb327a64f4fa7c53196f2
Natalia Ahmad <na906@nova.edu>
2024-02-22 20:35:24 -0500

    Added html & js code for DOM problem 1

---
+++ DOM-problems/DOM-problem 1/script.js
@@ -0,0 +1,3 @@
+function changeBackgroundColor() {                                    // function that is called when the button in clicked
+    document.getElementById("p1").style.backgroundColor = "red";      // I get the element in the HTML document that has the id of p1 (paragraph element)
+  }                                                                   // And, I set it's background color's style to red
