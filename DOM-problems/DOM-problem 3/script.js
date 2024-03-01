48f6d74f88672191efcc3833cb69b4ee52c711fe
Natalia Ahmad <na906@nova.edu>
2024-02-29 22:17:50 -0500

    Fixed JS code

--- DOM-problems/DOM-problem 3/script.js
+++ DOM-problems/DOM-problem 3/script.js
@@ -3,9 +3,10 @@ function outputItem() {                                                         
     document.getElementById("p1").innerHTML = "There are " + numOfItems + " options in the dropdown.";          // displaying the length of the drop-down menu in p1 paragraph element
      
     let optionsArray = [];                                                                                      // declaraing an array to push drop-down menu's values to
-   
-    for(i = 0; i < document.getElementById("drop-down").length; i++) {                                          // for loop to go through th length of the drop-down menu
-        optionsArray.push(document.getElementById("drop-down").options[i].text);                                // pushing teh text of each option at each index to the array
+    let options = document.getElementById("drop-down");
+
+    for(i = 0; i < options.length; i++) {                                                                       // for loop to go through th length of the drop-down menu
+        optionsArray.push(options.options[i].text);                                                             // pushing teh text of each option at each index to the array
     }
     optionsArray.toString();                                                                                    // converting the array into a string
     document.getElementById("p2").innerHTML = "Options are: " + optionsArray;                                   // displaying the string in p2 paragraph element
