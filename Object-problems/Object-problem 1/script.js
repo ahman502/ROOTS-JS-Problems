8448fc37f2c005b5feb3537df8fab35504a81431
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-03-18 21:55:34 -0400

    Added code and comments

--- Object-problems/Object-problem 1/script.js
+++ Object-problems/Object-problem 1/script.js
@@ -1,1 +1,27 @@
+const person = {                                        // creating a person object
+  firstName: "Natalia", 
+  lastName: "Ahmad", 
+  hairColor: "Black", 
+  eyeColor: "Brown", 
+  wearsGlasses: true,
+  age: 50
+}
+  
+function getPerson(obj, prop) {                         // function that takes in 2 arguments (object and key of the object)
+   delete obj[prop];                                    // deleting the object key using bracket notation
+   return obj;                                          // returning the object after deleting the key, value pair
+}
 
+console.log(getPerson(person, 'age'));                  // function call
+
+/*      OUTPUT                                          
+
+[object Object] {                                       // age key deleted
+  eyeColor: "Brown",
+  firstName: "Natalia",
+  hairColor: "Black",
+  lastName: "Ahmad",
+  wearsGlasses: true
+}                                           
+
+*/
