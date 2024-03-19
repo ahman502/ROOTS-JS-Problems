d6a7b86e07f0dee497edc5cdee7d7cf9847cef76
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-03-18 22:17:17 -0400

    Added code and comments to script.js file
    Create an array of people objects with first name, last name, and age, then write a JavaScript function that takes in an array and console logs the first and last names of all the people.

--- Object-problems/Object-problem 2/script.js
+++ Object-problems/Object-problem 2/script.js
@@ -1,1 +1,34 @@
+const Brooklyn99Characters = [                                                                     // creating an array of objects
+  {
+    firstName: "Amy",
+    lastName: "Santiago",
+    age: 28
+  },
+  {
+    firstName: "Jake",
+    lastName: "Peralta",
+    age: 32
+  }, 
+  {
+    firstName: "Charles",
+    lastName: "Boyle",
+    age: 35
+  }
+];
 
+function getNames(sampleArray) {                                                                   // function that takes an array as argument
+  for(let i = 0; i < sampleArray.length; i++) {                                                    // iterating over the array
+    console.log("Name: " + sampleArray[i]['firstName'] + " " + sampleArray[i]['lastName']);        // printing first and last names of each object within the array
+  }
+}
+
+getNames(Brooklyn99Characters);                                                                    // calling the function with sample array
+
+
+/*       OUTPUT
+
+"Name: Amy Santiago"
+"Name: Jake Peralta"
+"Name: Charles Boyle"
+
+*/
