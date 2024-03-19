304a023b7252705af1b33bb8fce18acfe6a5fd20
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-03-18 20:09:40 -0400

    Update script-v2.js

--- Array-problems/Array-problem 4/script-v2.js
+++ Array-problems/Array-problem 4/script-v2.js
@@ -1,10 +1,9 @@
 function checkDupes(array) {
-  return array.filter(check);
-}
-
-function check(val, index, testArray) {
-//   console.log("index of array: " + index + ", assigned index: " + testArray.indexOf(val));
-  return testArray.indexOf(val) === index;
+  let result = array.filter((val, index, array) => {
+    return array.indexOf(val) === index;
+  })
+  
+  return result;
 }
 
 console.log(checkDupes([1, 20, 3, 3, 5, 7, 4, 4, 2, 5, 3, 3, 20, 10, 4, 4, 10, 5, 6]));
