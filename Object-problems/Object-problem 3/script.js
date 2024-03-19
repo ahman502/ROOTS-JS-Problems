0aa7d9eeef93693d660f42993bfc794cc1805199
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-03-18 23:37:36 -0400

    Added code and comments to script.js
    Create an object with three items in it, then write a JavaScript function that accepts an object and tells you the length (tells you how many items are in it) of the object.

--- Object-problems/Object-problem 3/script.js
+++ Object-problems/Object-problem 3/script.js
@@ -1,19 +1,26 @@
-const sampleObject = 
+const sampleObject =                           // creating sample object with only 3 keys
   {
     item1: "Milk",
     item2: "Sugar",
     item3: "Rice"
   };
-
-function getLength(object) {
-  return Object.keys(object).length;
+  
+function getLength(obj) {                      // function that accepts object as argument     
+  if(typeof obj !== 'object') {                // checking if the arguments type is an object
+    return "Please use object input";          // if type is not an object, I print this
+  }
+  else {                                       // if type if object, I get the keys of the object as an array and return the length of that array
+    return Object.keys(obj).length;
+  }
 }
 
-console.log(getLength(sampleObject));
+console.log(getLength(sampleObject));          // function call with object as argument
+console.log(getLength(27));                    // function call with number as argument
 
 
 /*   OUTPUT    
 
-3
+3                                              => PRINTS THE LENGTH
+"Please use object input"                      => gives an error message
 
 */
