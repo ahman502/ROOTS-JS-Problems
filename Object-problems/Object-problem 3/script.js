d8ae07224fe906d5f726f3f805ad8158f827a170
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-03-18 23:30:40 -0400

    Update script.js

--- Object-problems/Object-problem 3/script.js
+++ Object-problems/Object-problem 3/script.js
@@ -1,1 +1,19 @@
+const sampleObject = 
+  {
+    item1: "Milk",
+    item2: "Sugar",
+    item3: "Rice"
+  };
 
+function getLength(object) {
+  return Object.keys(object).length;
+}
+
+console.log(getLength(sampleObject));
+
+
+/*   OUTPUT    
+
+3
+
+*/
