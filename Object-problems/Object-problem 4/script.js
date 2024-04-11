0e6e2c6d7e34a02c82eaae9b039a4f17efe46797
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-04-10 20:41:49 -0400

    Added code and comments to JS file

--- Object-problems/Object-problem 4/script.js
+++ Object-problems/Object-problem 4/script.js
@@ -1,1 +1,32 @@
+const person = {                                          // create person object
+  firstName: "Natalia", 
+  lastName: "Ahmad", 
+  hairColor: "Black", 
+  eyeColor: "Brown", 
+  wearsGlasses: true,
+  age: 50
+}
+ 
+function checkKey(obj, key) {                             // function that takes object and key as argument
+  if(obj.hasOwnProperty(key)) {                           // if the key exists in an object, return true
+    return obj.hasOwnProperty(key);
+  }
+  else {
+    return false;                                         // else return false
+  }
+}
 
+console.log(checkKey(person, 'hat'));                    // false
+console.log(checkKey(person, 'age'));                    // true
+console.log(checkKey(person, 'wearsGlasses'));           // true
+
+
+
+/*          OUTPUT            
+
+false
+true
+true
+
+
+*/
