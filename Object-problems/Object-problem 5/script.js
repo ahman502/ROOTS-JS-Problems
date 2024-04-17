7b9cf9698082a30ad5e20ce03a7163865ab2999a
Natalia Ahmad <61097833+ahman502@users.noreply.github.com>
2024-04-16 22:55:14 -0400

    Updated script.js

--- Object-problems/Object-problem 5/script.js
+++ Object-problems/Object-problem 5/script.js
@@ -1,26 +1,22 @@
-let sampleArray = [                                                                // test array
+let sampleArray = [                                                               // test array
    {author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
    {author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
-   {author: 'Suzanne Collins', title: 'Mockingjay: The Final Book 
-     of The Hunger Games', libraryID: 3245}
+   {author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
 ];
 
-sampleArr.sort((a, b) => {                                                        // using array sort function
-  
-  if(a.title > b.title) {                                                         // if the title value of a is greater than b's title value, we return 1 so a's title is placed after b's title
+function sortArr (a, b) {                              // comparator function
+  if (a.title > b.title) {                             // if the title value of a is greater than b's title value, we return 1 so a's title is placed after b's title        
     return 1;
   }
-  if(a.title < b.title) {                                                         // if the title value of a is lesser than b's title value, we return -1 so a's title is placed before b's title
+  else if (a.title < b.title) {                        // if the title value of a is lesser than b's title value, we return -1 so a's title is placed before b's title
     return -1;
   }
-  else {                                                                          // if both title values are the same, they remain in their position
-    return 0;
+  else {                                         
+    return 0;                                         // if both title values are the same, they remain in their position
   }
-});
+};
 
-console.log(sampleArray);                                                         // I print the sorted array
-
-
+console.log(sampleArray.sort(sortArr));               // I print the sorted array
 
 /*    OUTPUT               
 
